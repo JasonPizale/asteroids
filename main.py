939dcb00a5a8bdce8f8c542cc5c7be15c349b1fd
JasonPizale <jasonpizale@gmail.com>
2025-07-14 15:37:05 -0400

    cleaned up code using groups

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
-    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     clock = pygame.time.Clock()
     dt = 0
     while True:
@@ -19,11 +22,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for item_to_draw in drawable:
+            item_to_draw.draw(screen)
+            
         pygame.display.flip()
-        
 
 if __name__ == "__main__":
     main()
