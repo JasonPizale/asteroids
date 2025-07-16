00a2cef6e9a048e2ee97e581d4005dfb30151dc0
JasonPizale <jasonpizale@gmail.com>
2025-07-16 18:23:37 -0400

    added .rect to asteroid and the update method

--- asteroid.py
+++ asteroid.py
@@ -5,12 +5,18 @@ from constants import SHOT_RADIUS
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
-
+        self.rect = pygame.Rect(
+            self.position.x - self.radius,
+            self.position.y - self.radius,
+            2 * self.radius,
+            2 * self.radius
+        )
     def draw(self, screen): 
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
     def update(self, dt):
         self.position = self.position + (self.velocity * dt)
+        self.rect.center = (self.position.x, self.position.y)
 
 class Shot(CircleShape, pygame.sprite.Sprite):
     def __init__(self, position, velocity):
